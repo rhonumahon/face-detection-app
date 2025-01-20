905c33af8a36897164e6c7a7680a16422de0bc42
Rhon <rhonumahon@gmail.com>
2025-01-20 18:48:27 +0800

    FIX: Increase the data

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const EXTERNAL_API_URL = process.env.EXTERNAL_API_URL || 'https://face-2snbrq2o3
 const PORT = process.env.PORT || 8080;
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '10mb' })); // Increase the limit to 10MB or as required
 app.use(cors());
 app.use(morgan('combined'));
 
